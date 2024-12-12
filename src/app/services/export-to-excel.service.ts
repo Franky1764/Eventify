fb6bf89e116f08c126b1d75cd68f0a5960bd18c6
Mauricio <mpoblete.s3@gmail.com>
2024-12-12 16:48:50 -0300

    export events as Excel file and save

--- src/app/services/export-to-excel.service.ts
+++ src/app/services/export-to-excel.service.ts
@@ -1,6 +1,6 @@
 import * as XLSX from 'xlsx';
 import { Injectable } from '@angular/core';
-import { saveAs } from 'file-saver';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +8,7 @@ import { saveAs } from 'file-saver';
 export class ExportToExcelService {
   constructor() {}
 
-  exportAsExcel(data: any[], fileName: string): void {
+  async exportAsExcel(data: any[], fileName: string): Promise<void> {
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data);
     const workbook: XLSX.WorkBook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Eventos');
@@ -18,8 +18,25 @@ export class ExportToExcelService {
       type: 'array',
     });
 
-    // Guardar el archivo
     const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
-    saveAs(blob, `${fileName}.xlsx`);
+    const base64Data = await this.convertBlobToBase64(blob);
+
+    await Filesystem.writeFile({
+      path: `${fileName}.xlsx`,
+      data: base64Data,
+      directory: Directory.Documents,
+      encoding: Encoding.UTF8,
+    });
+  }
+
+  private convertBlobToBase64(blob: Blob): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
+      reader.readAsDataURL(blob);
+    });
   }
 }
